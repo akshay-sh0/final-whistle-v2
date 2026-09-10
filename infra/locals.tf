d555d5d14c6aed8528a2c8f3620072f8517c9a94
akshay shirodkar <akshaydshirodkar@gmail.com>
2026-09-10 13:01:13 +0100

    infra: add deployment method tag

--- infra/locals.tf
+++ infra/locals.tf
@@ -2,10 +2,11 @@ locals {
   name_suffix = "${var.workload}-${var.environment}"
 
   common_tags = {
-    workload    = var.workload
-    environment = var.environment
-    managed_by  = "terraform"
-    repository  = "final-whistle-v2"
-    cost_centre = "personal"
+    workload          = var.workload
+    environment       = var.environment
+    managed_by        = "terraform"
+    repository        = "final-whistle-v2"
+    cost_centre       = "personal"
+    deployment_method = "azure-devops"
   }
 }
