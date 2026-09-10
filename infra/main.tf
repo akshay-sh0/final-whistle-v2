0bca4835ca103b4d2f575acb58201e5feb7d2fae
akshay shirodkar <akshaydshirodkar@gmail.com>
2026-09-10 18:28:27 +0100

    infra: add resource group budget alerts

--- infra/main.tf
+++ infra/main.tf
@@ -12,4 +12,38 @@ resource "azurerm_static_web_app" "main" {
   sku_size            = "Free"
   tags                = local.common_tags
 
+  lifecycle {
+    ignore_changes = [
+      repository_branch,
+      repository_url,
+    ]
+  }
+}
+
+resource "azurerm_consumption_budget_resource_group" "main" {
+  name              = "budget-${local.name_suffix}"
+  resource_group_id = azurerm_resource_group.main.id
+
+  amount     = 500
+  time_grain = "Monthly"
+
+  time_period {
+    start_date = "2026-10-01T00:00:00Z"
+  }
+
+  notification {
+    enabled        = true
+    threshold      = 80
+    operator       = "GreaterThanOrEqualTo"
+    threshold_type = "Forecasted"
+    contact_emails = ["akshaydshirodkar@gmail.com"]
+  }
+
+  notification {
+    enabled        = true
+    threshold      = 100
+    operator       = "GreaterThanOrEqualTo"
+    threshold_type = "Actual"
+    contact_emails = ["akshaydshirodkar@gmail.com"]
+  }
 }
